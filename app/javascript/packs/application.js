1955606601ea279fcc63503370ce86f1fe2e4102
Faizan Haider <hfaizan53@gmail.com>
2022-06-28 00:03:48 +0500

    fix style issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "./bootstrap.bundle.min.js"
 import "./slider"
+import "./bootstrap.bundle.min.js"
+
 
 Rails.start()
 Turbolinks.start()
