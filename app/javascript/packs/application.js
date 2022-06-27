12ec6c4bf7034d48d8bb42a5b2eb36c1e0e24ca1
Faizan Haider <hfaizan53@gmail.com>
2022-06-27 23:16:19 +0500

    Implement new design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "./bootstrap.bundle.min.js"
+import "./slider"
 
 Rails.start()
 Turbolinks.start()
