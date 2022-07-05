9c4ff5505e6996c74842a20a6396d9d31c470626
Ansaissab <zahidensari116@gmail.com>
2022-07-05 20:45:43 +0500

    Added start page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:"pages#index"
 
+  get "start", to: "pages#start"
   get "privacy", to: "pages#privacy"
   get "terms", to: "pages#terms"
 end
